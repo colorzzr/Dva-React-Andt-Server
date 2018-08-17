723e727c0477a71263d89a737483647a8c86edf0
Ubuntu <ubuntu@ip-172-31-33-19.us-east-2.compute.internal>
2018-08-17 18:16:17 +0000

    switch server

--- src/components/Calculator/CalculatorComp.js
+++ src/components/Calculator/CalculatorComp.js
@@ -113,7 +113,7 @@ class CalculatorComp extends PureComponent {
     };
 
     // sending the request 47.96.95.207:8888
-    $.post('http://47.96.95.207:8888/calProcess', {
+    $.post('http://18.222.148.18:8888/calProcess', {
       first: JSON.stringify(obj),
     },
         (data) => {
