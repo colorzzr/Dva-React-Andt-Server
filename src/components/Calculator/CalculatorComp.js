07d28707ce459edc8921213c9b4190a180072e5c
colorzzr <colorzzr@gmai.com>
2018-09-02 04:25:31 +0800

    server para

--- src/components/Calculator/CalculatorComp.js
+++ src/components/Calculator/CalculatorComp.js
@@ -172,9 +172,9 @@ class CalculatorComp extends PureComponent {
       },
     };
 
-    // sending the request 47.96.95.207:8888
-    // $.post('http://18.222.148.18:8888/calProcess', {
-    $.post('http://localhost:8888/calProcess', {
+    // sending the request 
+    $.post('http://18.222.148.18:8888/calProcess', {
+    // $.post('http://localhost:8888/calProcess', {
       first: JSON.stringify(obj),
     },
         (data) => {
