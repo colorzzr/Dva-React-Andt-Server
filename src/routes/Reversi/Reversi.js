149f6ee41fc8ced312da1cab22c3c95725589cae
colorzzr <colorzzr@gmai.com>
2018-09-17 22:36:07 +0800

    adding the uesr and AI tag

--- src/routes/Reversi/Reversi.js
+++ src/routes/Reversi/Reversi.js
@@ -1,4 +1,6 @@
 import React, { PureComponent } from 'react';
+import $ from 'jquery';
+import { Row, Col } from 'antd';
 import styles from './Reversi.less';
 import whiteChess from '../../assets/Reversi/white chess.png';
 import blackChess from '../../assets/Reversi/black chess.png';
@@ -11,7 +13,7 @@ class ReversiGame extends PureComponent {
 
     this.state = {
       map: [],
-      ableMovePos: [],
+      movePos: [],
     };
   }
 
@@ -20,13 +22,13 @@ class ReversiGame extends PureComponent {
     for (let i = 0; i < 8; i += 1) {
       map[i] = [];
       for (let j = 0; j < 8; j += 1) {
-        map[i].push(-1);
+        map[i].push('_');
       }
     }
-    map[3][3] = 1;
-    map[3][4] = 0;
-    map[4][3] = 0;
-    map[4][4] = 1;
+    map[3][3] = 'B';
+    map[3][4] = 'W';
+    map[4][3] = 'W';
+    map[4][4] = 'B';
     console.log(map);
 
 // set up the initial plane
@@ -36,28 +38,57 @@ class ReversiGame extends PureComponent {
     });
   }
 
+  sendToBack(target) {
+    console.log(target.target.id);
+    const curMove = parseInt(target.target.id, 10);
+    const curMoveStr = (Math.floor(curMove / 8)).toString() + (curMove % 8).toString();
+    const { map } = this.state;
+
+    const obj = {
+      Move: curMoveStr,
+    };
+
+
+    $.post('http://localhost:8007/Reversi', {
+      first: JSON.stringify(obj),
+    },
+    (data) => {
+      console.log(data);
+    // change back to json
+      let sendBackData = JSON.parse(data);
+
+    // IDK why this is so magic that I need convert from []byte->string->json
+      sendBackData = JSON.parse(sendBackData);
+      const { Board } = sendBackData;
+
+      this.setState({
+        map: Board,
+      });
+    });
+  }
+
   render() {
     const { map } = this.state;
 
     const chessBoard = [];
     for (let i = 0; i < 8; i += 1) {
       for (let j = 0; j < 8; j += 1) {
-        if (map[i][j] === 1) {
+        if (map[i][j] === 'W') {
           chessBoard.push(
-            <li key={(i * 8) + j}>
+            <li key={(i * 8) + j} >
               <img src={whiteChess} alt="whiteChess" />
             </li>,
-);
-        } else if (map[i][j] === 0) {
+          );
+        } else if (map[i][j] === 'B') {
           chessBoard.push(
             <li key={(i * 8) + j}>
               <img src={blackChess} alt="blackChess" />
             </li>,
-);
+          );
         } else {
           chessBoard.push(
-            <li key={(i * 8) + j} />,
-);
+            <li key={(i * 8) + j} id={(i * 8) + j} onClick={this.sendToBack.bind(this)} />,
+          );
         }
       }
     }
@@ -65,7 +96,39 @@ class ReversiGame extends PureComponent {
     return (
       <div className={styles.wholeWindow}>
         <h1 className={styles.header}>ReversiGame</h1>
-        <h1 className={styles.header}> Point Vs Point </h1>
+        <div className={styles.headHolder}>
+          <Row>
+            <Col span={6}>
+              <Row>
+                <Col span={12}>
+                  <img className={styles.scoreBoardTag} src={blackChess} alt="blackChess" />
+                </Col>
+                <Col span={12}>
+                  <div className={styles.AIUserTag}>
+                    <p>User</p>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+
+            <Col span={12}>
+              <h1 className={styles.header}> Point Vs Point </h1>
+            </Col>
+
+            <Col span={6}>
+              <Row>
+                <Col span={12}>
+                  <img className={styles.scoreBoardTag} src={whiteChess} alt="whiteChess" />
+                </Col>
+                <Col span={12}>
+                  <div className={styles.AIUserTag}>
+                    <p>AI</p>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </div>
         <ul className={styles.chessplane}>
           {chessBoard}
         </ul>
